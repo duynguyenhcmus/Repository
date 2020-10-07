c4c2be0411e88bbe4446fcea90c74a691a3350f7
duynguyenhcmus <ndvduy@gmail.com>
2020-10-07 23:03:58 +0700

    Last updated

--- my-project/python-code.py
+++ my-project/python-code.py
@@ -2,14 +2,5 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-diamonds_url = "https://raw.githubusercontent.com/TrainingByPackt/Interactive-Data-Visualization-with-Python/master/datasets/diamonds.csv"
-diamonds_df = pd.read_csv(diamonds_url)
-diamonds_df['price_per_carat'] = diamonds_df['price']/diamonds_df['carat']
-diamonds_df['price_per_carat_is_high'] = np.where(diamonds_df['price_per_carat']>3500,1,0)
-diamonds_df['price']= diamonds_df['price']*1.3
-import math
-diamonds_df['rounded_price']=diamonds_df['price'].apply(math.ceil)
-print(diamonds_df.head())
-diamonds_df = sns.load_dataset('diamonds')
 print("this will be explosive!")
 
