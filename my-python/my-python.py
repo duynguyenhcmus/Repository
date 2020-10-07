3eb069f95f0187b9a90ccf4901481049d2a5773f
duynguyenhcmus <ndvduy@gmail.com>
2020-10-07 22:55:20 +0700

    updated

--- my-python/my-python.py
+++ my-python/my-python.py
@@ -1,5 +1,14 @@
 print("Hello world")
-import matplotlib.pyplot as plt
-array=[1,2,3,4,5,6]
-plt.plot(array)
-plt.show()
+import numpy as np
+number_of_steps=1000
+walks=np.random.randint(0,2,size=number_of_steps)
+position=0
+value=[]
+for i in range(number_of_steps):
+    if walks[i]==0:
+        position+=-1
+        value.append(position)
+    else:
+        position+=1
+        value.append(position)
+value
